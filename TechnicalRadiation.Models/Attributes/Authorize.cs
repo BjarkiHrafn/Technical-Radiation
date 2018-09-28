8bcde1b94871680121413e721c173fde8c01a603
BjarkiHrafn <bjarki15@ru.is>
2018-09-28 14:21:57 +0000

    g

--- TechnicalRadiation.Models/Attributes/Authorize.cs
+++ TechnicalRadiation.Models/Attributes/Authorize.cs
@@ -1,7 +1,17 @@
+using System.Web.Mvc;
+
 namespace TechnicalRadiation.Models.Attributes
 {
-    public class Authorize
+    public class Authorize : ActionFilterAttribute
     {
-        
+        string key = "";
+        string value = "";
+/* 
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            // pre-processing
+            Debug.WriteLine("ACTION 1 DEBUG pre-processing logging");
+        }
+*/
     }
 }
